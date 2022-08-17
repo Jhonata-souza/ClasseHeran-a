1e29b7175656d41ecd690a8fdce0fb5c2c63755a
Jhonata-souza <101649107+Jhonata-souza@users.noreply.github.com>
2022-08-17 11:35:23 -0300

    criando a classe aluno e professor

---
+++ Classeheranca/Classeheranca/Aluno.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classeheranca
+{
+    public class Aluno : Pessoa
+    {
+        public string curso;
+
+        public Aluno()
+        {
+            this.curso = null;
+        }
+
+        public Aluno(string nome, int idade, string curso)
+        {
+            this.Nome = nome;
+            this.Idade = idade;
+            this.curso = curso;
+        }
+
+        public string Titulacao
+        {
+            get { return curso; }
+            set { curso = value; }
+        }
+
+        public override string ImprimeDados()
+        {
+            String s = String.Empty;
+            s = String.Concat(this.Nome, " ", this.Idade.ToString(), "", this.curso);
+            return s;
+        }
+    }
+}
