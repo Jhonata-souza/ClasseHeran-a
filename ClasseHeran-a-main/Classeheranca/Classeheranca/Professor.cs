f1c8a9f57d52fc51aa806b0bee74ccab1c0713f2
Jhonata-souza <101649107+Jhonata-souza@users.noreply.github.com>
2022-08-19 12:12:14 -0300

    finalização do projeto
    configurando os botões dos forms

---
+++ ClasseHeran-a-main/Classeheranca/Classeheranca/Professor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classeheranca
+{
+    public class Professor : Pessoa
+    {
+        public string titulacao;
+
+        public Professor()
+        {
+            this.titulacao = null;
+        }
+
+        public Professor(string nome, int idade, string titulacao)
+        {
+            this.Nome = nome;
+            this.Idade = idade;
+            this.titulacao = titulacao;
+        }
+
+        public string Titulacao
+        {
+            get { return titulacao; }
+            set { titulacao = value; }
+        }
+
+        public override string ImprimeDados()
+        {
+            String s = String.Empty;
+            s = String.Concat(this.Nome, " ", this.Idade.ToString(), " ", this.Titulacao);
+            return s;
+        }
+    }
+}
---
+++ ClasseHeran-a-main/Classeheranca/Classeheranca/ProfessorCadastro.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Classeheranca
+{
+    public partial class ProfessorCadastro : Form
+    {
+        Professor professor;
+        public ProfessorCadastro()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(professor.ImprimeDados());
+        }
+
+        private void ProfessorCadastro_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            professor = new Professor(textBox1.Text, (Convert.ToInt32(textBox2.Text)), textBox3.Text);
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 form = new Form1();
+            form.Show();
+        }
+    }
+}
